d3934ee5ec6105e528e373fa6e7c5022e185a93b
nayeemmehedi <boyneel79@gmail.com>
2023-07-15 13:53:49 +0600

    pagination react perfectly working with backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Pagination } from '@mui/material';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(data.dataLength/ itemsPerPage);
+
+  console.log(data)
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/data?page=${currentPage}&limit=${itemsPerPage}`)
+      .then((response) => response.json())
+      .then((Data) => setData(Data))
+      .catch((error) => console.log(error));
+  }, [currentPage]);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Render your paginated data here */}
+      <ul>
+        {data?.paginatedData?.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+
+      {/* Render the pagination component */}
+      <Pagination
+        count={totalPages}
+        page={currentPage}
+        onChange={handlePageChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
